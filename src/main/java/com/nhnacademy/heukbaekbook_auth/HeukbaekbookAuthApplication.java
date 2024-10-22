49e5f4e3eb41ccc69d4534808b3a80f804fbeaf6
wjdtjdgns <person3275@gmail.com>
2024-10-22 09:49:56 +0900

    init

---
+++ src/main/java/com/nhnacademy/heukbaekbook_auth/HeukbaekbookAuthApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.heukbaekbook_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeukbaekbookAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeukbaekbookAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhnacademy/heukbaekbook_auth/HeukbaekbookAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.heukbaekbook_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeukbaekbookAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
