bc9ceeb6a270a2fbd578ff8c01f01a7926862e0e
wjdtjdgns <person3275@gmail.com>
2024-10-24 09:38:11 +0900

    [Feat] 도메인 추가

---
+++ src/main/java/com/nhnacademy/heukbaekbook_auth/domain/Member.java
@@ -0,0 +1,42 @@
+package com.nhnacademy.heukbaekbook_auth.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "members")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long customerId;
+
+    @Column(name = "grade_id")
+    private Long gradeId;
+
+    @Column(name = "member_login_id")
+    private String loginId;
+
+    @Column(name = "member_password")
+    private String memberPassword;
+
+    @Column(name = "member_birth")
+    private LocalDate memberBirth;
+
+    @Column(name = "member_created_at")
+    private LocalDateTime memberCreatedAt;
+
+    @Column(name = "member_last_login_at")
+    private LocalDate memberLastLoginAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "member_status")
+    private MemberStatus status;
+}
