e64585ef24c0de0f05a15e3eb3df13298c0ae7f1
jd-hyun <wjdehdgus3540@naver.com>
2024-11-15 15:17:47 +0900

    [Fix] 무중단 배포 수정 - #26

--- src/main/java/com/nhnacademy/heukbaekbook_auth/config/CorsMvcConfig.java
+++ src/main/java/com/nhnacademy/heukbaekbook_auth/config/CorsMvcConfig.java
@@ -9,8 +9,9 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("https://www.heukbaekbook.shop")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
