89ed8aeec0c1ab5cdc3fba2fbc6237087dbe6cbe
jd-hyun <wjdehdgus3540@naver.com>
2024-11-15 16:18:31 +0900

    [Fix] 무중단 배포 수정 - #26

--- src/main/java/com/nhnacademy/heukbaekbook_auth/config/CorsMvcConfig.java
+++ src/main/java/com/nhnacademy/heukbaekbook_auth/config/CorsMvcConfig.java
@@ -4,14 +4,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsMvcConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
-}
+//@Configuration
+//public class CorsMvcConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOriginPatterns("*")
+//                .allowedMethods("*")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+//    }
+//}
